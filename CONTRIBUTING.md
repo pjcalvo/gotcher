a2739e0e186f8c556b923e823f38988716b05690
pablo.calvo <pjcalvov@gmail.com>
2023-05-16 16:41:34 +0200

    add readme

---
+++ CONTRIBUTING.md
@@ -0,0 +1,12 @@
+# Contributing to this repo
+
+## Todo list
+
+- parameterize authentication based on config
+- change config to patch based on params and METHOD type
+- parameterize CORS
+- accept verbose as a flag or read it from file
+- parameterize the file to be used
+- de-duplicate code from the shoulds functions
+- add README + docs
+- release as a binary
--- main.go
+++ main.go
@@ -17,17 +17,6 @@ import (
 // single config
 var patchConfig *config.Config
 
-/*
-	 Todo list:
-		- parameterize authentication based on config
-		- change config to patch based on params and METHOD type
-		- parameterize CORS
-		- accept verbose as a flag or read it from file
-		- parameterize the file to be used
-		- de-duplicate code from the shoulds functions
-		- add README + docs
-		- release as a binary
-*/
 func main() {
 	cons, err := config.LoadConfig()
 	if err != nil {
